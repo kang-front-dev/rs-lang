a5a900fbda49015d398274217d27373513620115
alexanderson0708 <sonalexander0708@gmail.com>
2022-02-13 21:48:33 +0500

    fix: rename folder

--- rs-lang/src/index.ts
+++ rs-lang/src/index.ts
@@ -8,12 +8,3 @@ import './components/pages/register/register.css';
 
 const app = new App();
 app.run();
-
-
-
-
-// async function asd(){
-//     const a = await getWord("5e9f5ee35eb9e72bc21af70c")
-//     console.log(a)
-// }
-// asd()
