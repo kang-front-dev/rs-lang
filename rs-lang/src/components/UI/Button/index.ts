c41f2bfbf0c528caf1a7b20c30d27d0ecca7a9ae
elizavetachizh <75884592+elizavetachizh@users.noreply.github.com>
2022-02-14 17:07:06 +0300

    feat: add  components button

---
+++ rs-lang/src/components/UI/Button/index.ts
@@ -0,0 +1,32 @@
+import "./button.scss";
+import { Component } from "../../pages/textBook/addition/addComponents";
+import "../../groupWords/style.scss"
+
+export class Button extends Component {
+  onClickButton: () => void = () => {};
+  constructor(
+    parentNode: HTMLElement,
+    styles: string[] = [],
+    content: string,
+    disabled = false
+  ) {
+    super(parentNode, "button", ["btn"], content);
+
+    this.element.classList.add(...styles);
+    this.element.addEventListener("click", () => this.onClickButton());
+
+    if (disabled) {
+      this.setDisabled(true);
+    }
+  }
+
+  setDisabled(type = false): void {
+    this.element.toggleAttribute("disabled", type);
+  }
+
+  removeDisabled(): void {
+    this.element.removeAttribute("disabled");
+  }
+
+  
+}
